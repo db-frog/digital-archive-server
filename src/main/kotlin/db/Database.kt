d62364f168bca1d854f2da0792552133e08cb231
Daniel Benett <deb2174@columbia.edu>
2024-09-23 12:16:32 -0700

    Code for inserting from csv

--- src/main/kotlin/db/Database.kt
+++ src/main/kotlin/db/Database.kt
@@ -7,8 +7,46 @@ import io.ktor.server.config.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.flywaydb.core.Flyway
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.io.FileInputStream
+import java.io.InputStream
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+object File : Table() {
+    val s3Path: Column<String> = varchar("s3_path", 512)
+    val text: Column<String> = text("text")
+    val topic: Column<Int?> = (integer("topic")).nullable()
+
+    override val primaryKey = PrimaryKey(s3Path) // name is optional here
+}
+
+operator fun <T> List<T>.component6() = this[5]
+
+data class LoreFile(
+    val s3Path: String,
+    val folder: String,
+    val topic: Int?,
+    val topicFullName: String,
+    val keyWords: String,
+    val text: String
+)
+
+fun getResourceFilePath(dataFileName: String) =
+    Paths.get(Paths.get("").toAbsolutePath().toString(), "/src/main/resources/db/data/$dataFileName")
+
+fun readCsv(inputStream: InputStream): List<LoreFile> {
+    val reader = inputStream.bufferedReader()
+    return reader.lineSequence()
+        .filter { it.isNotBlank() }
+        .map {
+            val (s3Path, folder, topic, topicFullName,  keyWords, text) = it.split(',', ignoreCase = false, limit = 6)
+            LoreFile(s3Path, folder, topic.toIntOrNull(), topicFullName, keyWords, text)
+        }.toList()
+}
 
 class Database {
     private val appConfig = HoconApplicationConfig(ConfigFactory.load())
@@ -21,8 +59,21 @@ class Database {
 
     init {
         Database.connect(hikari())
+
         val flyway = Flyway.configure().dataSource(dbUrl, dbUser, dbPassword).load()
         flyway.migrate()
+
+        /*transaction {
+            SchemaUtils.create(File)
+            val dataFile = Files.newInputStream(getResourceFilePath("filetexttopic.csv"))
+            readCsv(dataFile).forEach { loreFile ->
+                File.insert {
+                    it[s3Path] = loreFile.s3Path
+                    it[text] = loreFile.text
+                    it[topic] = loreFile.topic
+                }
+            }
+        }*/
     }
 
     private fun hikari(): HikariDataSource {
